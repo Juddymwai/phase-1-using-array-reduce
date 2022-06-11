2f03d0cc3a03ca7d780112093cd099669b2283e2
Judy Mwai <juddiemukami@gmail.com>
2022-06-11 19:28:21 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+
+
+// function addBatches(batteryBatches){
+//     let totalBatteries=0
+//     for (batch of batteryBatches){
+//         totalBatteries+= batch
+        
+//     }
+//     return totalBatteries
+
+// }
+// console.log(addBatches(batteryBatches))
+
+const totalBatteries= [4, 5, 3, 4, 4, 6, 5].reduce(addBatches)
+
+function addBatches(total, batches){
+    let totalBatteries=0
+    totalBatteries = batches + total
+    return totalBatteries;
+}
